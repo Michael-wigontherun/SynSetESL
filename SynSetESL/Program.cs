ce416c899dd9a1aeda1593de30e8293ed4870c65
ssebaa <60935038+ssebaa@users.noreply.github.com>
2024-03-31 19:27:24 +0200

    Update Program.cs

--- SynSetESL/Program.cs
+++ SynSetESL/Program.cs
@@ -30,7 +30,7 @@ namespace SynSetESL
                 if (rec.FormKey.ID > uintMax) isESPFE = false;
             }
 
-            if (isESPFE) state.PatchMod.ModHeader.Flags |= SkyrimModHeader.HeaderFlag.LightMaster;
+            if (isESPFE) state.PatchMod.ModHeader.Flags |= SkyrimModHeader.HeaderFlag.Light;
         }
     }
 }
